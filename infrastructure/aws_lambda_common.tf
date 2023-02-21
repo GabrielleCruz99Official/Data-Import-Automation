cb0ba7504403f3c1bb6548f8ac2a1e6032dd5ee3
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-02-21 14:29:24 +0100

    ADD infra modules and roles for inbound lambda

---
+++ infrastructure/aws_lambda_common.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "${var.project}-${var.env}-logging"
+  path        = "/"
+  description = "IAM policy for logging from lambdas"
+
+  policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": [
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents",
+                    "logs:CreateLogGroup"
+                ],
+                "Resource": "arn:aws:logs:*:*:*",
+                "Effect": "Allow"
+            }
+        ]
+    }
+    EOF
+}
+
+resource "aws_iam_policy" "lambda_s3_pol" {
+  name        = "${var.project}-${var.env}-lambda_s3_policy"
+  path        = "/"
+  description = "IAM policy for writing/reading SQS from a lambda"
+
+  policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+               "Sid": "",
+               "Effect": "Allow",
+               "Action": "s3:*",
+               "Resource": "*"
+            }
+        ]
+    }
+    EOF
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,8 +13,43 @@ variable "project" {
   type    = string
 }
 
+variable "bump_version" {
+  type        = string
+  description = "Function version"
+  default     = false
+}
+
+variable "handler_inbound" {
+  type        = string
+  description = "Handler for calling serverless function"
+  default     = "inbound.process_event"
+}
+
+variable "runtime" {
+  type        = string
+  description = "The handler runtime version for serverless invocation"
+  default     = "python3.8"
+}
+
+variable "s3_bucket" {
+  type        = string
+  description = "The S3 bucket that holds the packaged function"
+}
+
+variable "s3_key_inbound" {
+  type        = string
+  description = "The default key for zip artifact on s3 bucket"
+  default     = "default-artifact"
+}
+
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
   default     = "DataImportAutomation"
+}
+
+variable "function_debug_level" {
+  type        = string
+  description = "Debug level of the lambdas"
+  default     = "DEBUG"
 }
