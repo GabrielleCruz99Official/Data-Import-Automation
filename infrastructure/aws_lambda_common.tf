6fac6b4e25b5b0da4c2796f6781c95085d368672
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-02-24 11:40:37 +0100

    ADD sqs queue to infrastructure

--- infrastructure/aws_lambda_common.tf
+++ infrastructure/aws_lambda_common.tf
@@ -22,6 +22,26 @@ resource "aws_iam_policy" "lambda_logging" {
 EOF
 }
 
+resource "aws_iam_policy" "lambda_sqs_pol" {
+  name        = "${var.project}-${var.env}-lambda_sqs_policy_tfe"
+  path        = "/"
+  description = "IAM policy for writing/reading SQS from a lambda"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": "sqs:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_policy" "lambda_s3_pol" {
   name        = "${var.project}-${var.env}-lambda_s3_policy"
   path        = "/"
--- infrastructure/aws_s3_csv_bucket.tf
+++ infrastructure/aws_s3_csv_bucket.tf
@@ -8,3 +8,16 @@ resource "aws_s3_bucket" "csv_bucket" {
 
   force_destroy = true
 }
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id = "AllowExecutionFromS3Bucket"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.inbound.arn
+  principal = "s3.amazonaws.com"
+  source_arn = aws_s3_bucket.csv_bucket.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3"{
+  role = aws_iam_role.migration_role.name
+  policy_arn = aws_iam_policy.lambda_s3_pol.arn
+}
